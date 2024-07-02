8f96cf33bddc1e77e4628b0709cf8d7992f13c9d
Lamia Fatiha <f6lamia@gmail.com>
2024-07-02 20:20:44 +0600

    html done

---
+++ src/components/LoginSign.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import './style.css';
+import email from '../assets/image/email.png'
+import password from '../assets/image/password.png'
+import person from '../assets/image/person.png'
+
+
+const LoginSign = () => {
+  return (
+    <div className='container'>
+     <div className="header">
+        <div className="text">Sign Up</div>
+        <div className="underline"></div>
+     </div>
+     <div className="inputs">
+        <div className="input">
+            <img src={person} alt="icon" />
+            <input type="text"  />
+        </div>
+        <div className="input">
+            <img src={email} alt="icon" />
+            <input type="email"  />
+        </div>
+        <div className="input">
+            <img src={password} alt="icon" />
+            <input type="password"  />
+        </div>
+     </div>
+
+    </div>
+  )
+}
+
+export default LoginSign
